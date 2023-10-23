a1e89a373e43a5d5c3b472d200510f8f2d9d44ba
Eric Charles <eric@datalayer.io>
2023-10-23 11:27:29 +0200

    chore: require plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { treatAsCommonjs } from "vite-plugin-treat-umd-as-commonjs";
+// import vitePluginRequire from "vite-plugin-require";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     treatAsCommonjs(),
+//     vitePluginRequire.default(),
   ],
   resolve: {
     alias: [
